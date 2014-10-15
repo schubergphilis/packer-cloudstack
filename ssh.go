bd6eb0a5b347d5f83308872f35c1ec651d9e32d7
Andrew Bayer <andrew.bayer@gmail.com>
2014-10-15 17:03:48 -0400

    Add ability to specify a hardcoded password

--- ssh.go
+++ ssh.go
@@ -4,6 +4,8 @@ import (
 	"code.google.com/p/go.crypto/ssh"
 	"fmt"
 	"github.com/mitchellh/multistep"
+	commonssh "github.com/mitchellh/packer/common/ssh"
+	packerssh "github.com/mitchellh/packer/communicator/ssh"
 )
 
 func sshAddress(state multistep.StateBag) (string, error) {
@@ -16,15 +18,23 @@ func sshConfig(state multistep.StateBag) (*ssh.ClientConfig, error) {
 	config := state.Get("config").(config)
 	privateKey := state.Get("ssh_private_key").(string)
 
-	signer, err := ssh.ParsePrivateKey([]byte(privateKey))
-	if err != nil {
-		return nil, fmt.Errorf("Error setting up SSH config: %s", err)
+	auth := []ssh.AuthMethod{
+		ssh.Password(config.SSHPassword),
+		ssh.KeyboardInteractive(
+			packerssh.PasswordKeyboardInteractive(config.SSHPassword)),
+	}
+
+	if privateKey != "" {
+		signer, err := ssh.ParsePrivateKey([]byte(privateKey))
+		if err != nil {
+			return nil, fmt.Errorf("Error setting up SSH config: %s", err)
+		}
+
+		auth = append(auth, ssh.PublicKeys(signer))
 	}
 
 	return &ssh.ClientConfig{
 		User: config.SSHUsername,
-		Auth: []ssh.AuthMethod{
-			ssh.PublicKeys(signer),
-		},
+		Auth: auth,
 	}, nil
 }
